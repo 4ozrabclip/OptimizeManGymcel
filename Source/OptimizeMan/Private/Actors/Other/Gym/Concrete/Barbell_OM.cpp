4704477062527e5e8979ff5f60f1f9f0dc6661f7
Charlie Simmons <4oz.developer@gmail.com>
2025-07-10 00:41:02 +1200

    Copyright comments

--- Source/OptimizeMan/Private/Actors/Other/Gym/Concrete/Barbell_OM.cpp
+++ Source/OptimizeMan/Private/Actors/Other/Gym/Concrete/Barbell_OM.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright © 2025 4ozStudio. All rights reserved.
 
 
 #include "Actors/Other/Gym/Concrete/Barbell_OM.h"
