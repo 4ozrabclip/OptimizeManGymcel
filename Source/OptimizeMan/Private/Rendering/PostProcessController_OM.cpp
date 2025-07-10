df6fa9c6ceb189b49de1163ad936ec34b97a9a4c
Charlie Simmons <4oz.developer@gmail.com>
2025-07-10 23:40:05 +1200

    PostProcessSettings & Chromatic Abberation Tick

--- Source/OptimizeMan/Private/Rendering/PostProcessController_OM.cpp
+++ Source/OptimizeMan/Private/Rendering/PostProcessController_OM.cpp
@@ -3,6 +3,7 @@
 
 #include "Rendering/PostProcessController_OM.h"
 
+#include "Chaos/Character/CharacterGroundConstraintContainer.h"
 #include "Game/Persistent/GameInstance_OM.h"
 
 
@@ -14,6 +15,8 @@ APostProcessController_OM::APostProcessController_OM()
 	LightModeMaterial = nullptr;
 	DarkModeMID = nullptr;
 	LightModeMID = nullptr;
+	GameInstance = nullptr;
+
 }
 
 void APostProcessController_OM::BeginPlay()
@@ -50,6 +53,7 @@ void APostProcessController_OM::BeginPlay()
 	GameInstance->OnDarkModeToggled.AddDynamic(this, &APostProcessController_OM::SetDarkMode);
 
 	CheckDarkMode();
+
 }
 
 
@@ -71,4 +75,45 @@ void APostProcessController_OM::SetDarkMode(bool bDarkMode)
 			Blendable.Weight = bDarkMode ? 0.f : 1.f;
 		}
 	}
+}
+
+void APostProcessController_OM::StartChromaticEffects()
+{
+	int EndTime = FMath::RandRange(2, 20);
+	ChromaticTickCounter = 0.f;
+	FPostProcessSettings& Settings = GlobalPostProcessVolume->Settings;
+	Settings.bOverride_SceneFringeIntensity = true;
+	Settings.bOverride_ChromaticAberrationStartOffset = true;
+	GetWorld()->GetTimerManager().ClearTimer(ChromaticTickHandle);
+	GetWorld()->GetTimerManager().SetTimer(
+		ChromaticTickHandle,
+		[this, EndTime, &Settings]()
+		{
+			ChromaticEffectsTick(EndTime, Settings);
+		},TickRate,true);
+}
+
+void APostProcessController_OM::ChromaticEffectsTick(const int EndTime, FPostProcessSettings& Settings)
+{
+	if (Settings.SceneFringeIntensity < MaxChromaticIntensity)
+	{
+		Settings.SceneFringeIntensity = FMath::FInterpConstantTo(
+		Settings.SceneFringeIntensity, MaxChromaticIntensity, 0.065, 0.5);
+	}
+	if (bGoingUp)
+	{
+		if (Settings.ChromaticAberrationStartOffset >= MaxChromaticOffset) bGoingUp = false;
+		Settings.ChromaticAberrationStartOffset = FMath::FInterpConstantTo(
+	Settings.ChromaticAberrationStartOffset, MaxChromaticOffset, 0.065, 0.5);
+	}
+	else
+	{
+		if (Settings.ChromaticAberrationStartOffset <= MinChromaticOffset) bGoingUp = true;
+		Settings.ChromaticAberrationStartOffset = FMath::FInterpConstantTo(
+Settings.ChromaticAberrationStartOffset, MinChromaticOffset, 0.065, 0.5);
+	}
+	
+	ChromaticTickCounter += TickRate;
+	if (ChromaticTickCounter >= EndTime)
+		GetWorld()->GetTimerManager().ClearTimer(ChromaticTickHandle);
 }
--- Source/OptimizeMan/Public/Rendering/PostProcessController_OM.h
+++ Source/OptimizeMan/Public/Rendering/PostProcessController_OM.h
@@ -23,8 +23,34 @@ protected:
 	UFUNCTION()
 	void SetDarkMode(bool bDark);
 
+	UFUNCTION(BlueprintCallable)
+	void StartChromaticEffects();
+
+
+	void ChromaticEffectsTick(const int EndTime, FPostProcessSettings& Settings);
+
+	
+	/** Serialized Effect Params **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	float MaxChromaticIntensity = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	float MinChromaticOffset = 0.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	float MaxChromaticOffset = 1.f;
+
+	bool bGoingUp = true;
+	
+
+private:
+	float TimeSinceAnxietyTickStarted = 0.f;
+	int AnxietyTickEndTime = 0;
+	const float TickRate = 0.1f;
+
+	float ChromaticTickCounter = 0.f;
+
 	UPROPERTY()
 	UGameInstance_OM* GameInstance;
+
 
 protected:
 	UPROPERTY(EditInstanceOnly, Category="PostProcess")
@@ -40,5 +66,23 @@ protected:
 	UPROPERTY()
 	UMaterialInstanceDynamic* LightModeMID;
 
+	
+
+private:
+	FTimerHandle AnxietyTickHandle;
+	FTimerHandle ChromaticTickHandle;
+
+
+	float TargetFilmSlope;
+	float FilmSlopeFadeSpeed;
+	
+	float TargetVignette;
+	float VignetteFadeSpeed;
+
+	float TargetBloom;
+	float BloomFadeSpeed;
+
+	float TargetLensDistortion;
+	float LensDistortionFadeSpeed;
 
 };
